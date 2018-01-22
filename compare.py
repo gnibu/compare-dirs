0447c09e2316d6cb387e5352532b3e2c9c55d217
Benoit Pothier <ben@niak.net>
2018-01-22 21:24:52 +0100

    first working version

--- compare.py
+++ compare.py
@@ -1,5 +1,30 @@
 import os
+import hashlib
 
-for root, dirs, files in os.walk('/Volumes/photos/2011'):
-    for mfile in files:
-        print(root+"/"+ mfile)
+def hash_bytestr_iter(bytesiter, hasher, ashexstr=False):
+    for block in bytesiter:
+        hasher.update(block)
+    return (hasher.hexdigest() if ashexstr else hasher.digest())
+
+def file_as_blockiter(afile, blocksize=65536):
+    with afile:
+        block = afile.read(blocksize)
+        while len(block) > 0:
+            yield block
+            block = afile.read(blocksize)
+
+def list_hash(folder):
+    hashes = {}
+    for root, dirs, files in os.walk(folder):
+        print('indexing ' + root)
+        for mfile in files:
+            path = root+"/"+mfile
+            hashes[hash_bytestr_iter(file_as_blockiter(open(path, 'rb')), hashlib.sha256(), ashexstr=True)] = path
+    return hashes
+
+a=list_hash('/Volumes/backup/sort/photos-sauron/2010')
+b=list_hash('/Volumes/photos/2010')
+
+print('only in a')
+for hash in list(set(a.keys()) - set(b.keys())):
+    print(a[hash])
